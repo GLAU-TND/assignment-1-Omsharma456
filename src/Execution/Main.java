3b021642bbc335b26bbef0f0e61b7b99525ae949
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-02-24 21:53:57 +0530

    Creating user to show given format to add, remove, search exit Operation

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -6,6 +6,15 @@ import Definition.LinkedListDefinitionClass;
 public class Main {
     public static void main(String[] args) {
         LinkedListDefinitionClass<Client> listContact = new LinkedListDefinitionClass<Client>();
-
+        boolean turnoff = false;
+        int a = 0;
+        System.out.println("Welcome to Abhi Contact list \uD83D\uDE01:");
+        while (!turnoff) {
+            System.out.println("Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+        }
     }
 }
