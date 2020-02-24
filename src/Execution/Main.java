6c8f200324d2fd0766d515a8893dd19dcd424bb7
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-02-24 21:52:17 +0530

    Creating LinkedListDefinitionClass Object of type 'Client'

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -1,7 +1,11 @@
 package Execution;
 
+import Client.Client;
+import Definition.LinkedListDefinitionClass;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedListDefinitionClass<Client> listContact = new LinkedListDefinitionClass<Client>();
 
     }
 }
