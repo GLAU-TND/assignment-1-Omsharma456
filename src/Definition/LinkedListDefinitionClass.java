ed3a3758f049ad26dea3c5bfc8eb70045f027629
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-02-24 21:13:16 +0530

    Implementing all method (abstract) to give some function

--- src/Definition/LinkedListDefinitionClass.java
+++ src/Definition/LinkedListDefinitionClass.java
@@ -1,4 +1,23 @@
 package Definition;
 
 public class LinkedListDefinitionClass<E> implements LinkedListADTInterface<E> {
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
