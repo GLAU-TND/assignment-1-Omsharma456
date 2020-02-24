2d874b6327ada115d0be816d96e375f83302d90e
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-02-24 21:34:46 +0530

    Creating a variable Contact Number which store contact of user

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,6 +1,10 @@
 package Client;
 
+import Definition.LinkedListDefinitionClass;
+
 public class Client {
     private String firstName;
     private String lastName;
+    private LinkedListDefinitionClass<Long> contactNo;
+
 }
